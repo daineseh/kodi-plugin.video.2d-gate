d4226862cf3e297f25f9b58dbcf97d935d974ebe
Dainese Hsiao <daineseh@gmail.com>
2016-05-04 15:16:01 +0800

    v1.0.0

---
+++ plugin.video.2d-gate/addon.py
@@ -0,0 +1,106 @@
+#-*- coding: utf-8 -*-
+import sys
+import urllib
+import urlparse
+import xbmc
+import xbmcgui
+import xbmcplugin
+import youtube_dl
+
+from clawler import ParserAnimeFrom2dGate, ProcessEpisodes
+
+
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+
+def page_number(url):
+    return url.split('-')[-1].split('.')[0]
+
+mode = args.get('mode', None)
+
+if mode is None:
+    obj = ParserAnimeFrom2dGate()
+    for item in obj.get_classes():
+        url = build_url({'mode': 'week_folder', 'folder_name': item})
+        li = xbmcgui.ListItem(item, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                    listitem=li, isFolder=True)
+
+    target_url = obj.get_target_url()
+    li = xbmcgui.ListItem('')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=None, listitem=li, isFolder=False)
+
+    url = build_url({'mode': 'page', 'folder_name': target_url})
+    li = xbmcgui.ListItem("Next Page (%s) >> ┏┃^0^┃┛ >> ᕕ(╯°□°)ᕗ >> ┌( ಠ‿ಠ)┘ >> ε＝┏(･ω･)┛ >> " % page_number(target_url))
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'week_folder':
+    obj = ParserAnimeFrom2dGate()
+    folder_name = args['folder_name'][0]
+
+    for anime_name in obj.get_content(folder_name):
+        thumbnail = obj.get_thumb(folder_name, anime_name)
+        url = build_url({'mode': 'anime_info', 'folder_name': obj.get_url(folder_name, anime_name)})
+        li = xbmcgui.ListItem(anime_name)
+        li.setArt({'thumb': thumbnail, 'icon': thumbnail, 'fanart': thumbnail})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                    listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'page':
+    obj = ParserAnimeFrom2dGate()
+    page_name = args['folder_name'][0]
+
+    for anime in obj.get_page(page_name):
+        url = build_url({'mode': 'anime_info', 'folder_name': anime.url})
+        li = xbmcgui.ListItem(anime.name)
+        li.setArt({'thumb': anime.thumb, 'icon': anime.thumb, 'fanart': anime.thumb})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    if obj.next_page:
+        li = xbmcgui.ListItem('')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=None, listitem=li, isFolder=False)
+
+        url = build_url({'mode': 'page', 'folder_name': obj.next_page})
+        li = xbmcgui.ListItem("Next Page (%s) >> ┏┃^0^┃┛ >> ᕕ(╯°□°)ᕗ >> ┌( ಠ‿ಠ)┘ >> ε＝┏(･ω･)┛ >> " % page_number(obj.next_page) * 2)
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    if obj.last_page:
+        url = build_url({'mode': 'page', 'folder_name': obj.last_page})
+        li = xbmcgui.ListItem("Last Page (%s) >> ┏┃^0^┃┛ >> ᕕ(╯°□°)ᕗ >> ┌( ಠ‿ಠ)┘ >> ε＝┏(･ω･)┛ >> " % page_number(obj.last_page))
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'anime_info':
+    anime_url = args['folder_name'][0]
+    ep = ProcessEpisodes(anime_url)
+
+    for episode in ep.get_list():
+        url = build_url({'mode': 'play', 'folder_name': episode.url})
+        li = xbmcgui.ListItem(episode.name)
+        li.setArt({'thumb': episode.thumb, 'icon': episode.thumb, 'fanart': episode.background})
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'play':
+    url = args['folder_name'][0]
+    opts = {
+        'forceurl': True,
+        'quiet': True,
+        'simulate': True,
+    }
+
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        resource_uri = ydl.extract_info(url).get('url')
+        if not resource_uri:
+            entries = ydl.extract_info(url).get('entries')
+            resource_uri = entries[-1].get('url')
+    xbmc.Player().play(resource_uri)
+
---
+++ plugin.video.2d-gate/clawler.py
@@ -0,0 +1,338 @@
+# -*- coding: utf8 -*-
+
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+
+import requests
+import urlparse
+
+
+GOOGLE_URL = 'https://drive.google.com/file/d/'
+BUILD_GOOGLE_URL = lambda x: urlparse.urljoin(GOOGLE_URL, x).encode('utf8')
+
+BASE_URL = 'http://2d-gate.org'
+BUILD_URL = lambda x: urlparse.urljoin(BASE_URL, x).encode('utf8')
+
+
+class Episode:
+    def __init__(self, name = None, url = None,
+                 thumb = None, background = None):
+        self.name = self._to_str_type(name)
+        self.url = self._to_str_type(url)
+        self.thumb = self._to_str_type(thumb)
+        self.background = self._to_str_type(background)
+
+    def _to_str_type(self, value):
+        if not value:
+            return
+        if isinstance(value, str):
+            return value
+        return value.encode('utf8')
+
+    @property
+    def background(self):
+        return self.__background
+
+    @background.setter
+    def background(self, value):
+        self.__background = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def thumb(self):
+        return self.__thumb
+
+    @thumb.setter
+    def thumb(self, value):
+        self.__thumb = value
+
+    @property
+    def url(self):
+        return self.__url
+
+    @url.setter
+    def url(self, value):
+        self.__url = value
+
+
+class ProcessEpisodes():
+    def __init__(self, url):
+        self.url = url
+        self.episodes_list = []
+        self.__parse()
+
+    def __parse_arch_1(self, tag):
+        gd_thumb = tag.find('div', 'gd_thumb')
+        if not gd_thumb:
+            return None, None, None
+
+        onclick = gd_thumb.get('onclick')
+        target = onclick.split(',')[1]
+        url = BUILD_GOOGLE_URL(target.strip('\''))
+
+        style = gd_thumb.get('style')
+        if not style:
+            return None, None, None
+        l_paren = style.rfind('(')
+        r_paren = style.rfind(')')
+        background = BUILD_URL(style[l_paren + 1:r_paren])
+
+        img = gd_thumb.find('img')
+        if not img:
+            return None, None, None
+        thumb = BUILD_URL(img.get('src'))
+        return url, thumb, background
+
+    def __parse_arch_2(self, tag):
+        tag_a = tag.find('a')
+        if not tag_a:
+            return None, None, None
+        url = tag_a.get('href')
+        if not url:
+            return None, None, None
+        img = tag_a.find('img')
+        if not img:
+            return None, None, None
+        thumb = background = BUILD_URL(img.get('src'))
+        # Skip fake tab
+        if url == thumb:
+            return None, None, None
+        return url, thumb, background
+
+    def __parse(self):
+        req = requests.get(self.url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        content = soup.find('div', style="display:none")
+        if not content:
+            url, thumb, background = self.__parse_arch_1(soup)
+            if url and thumb and background:
+                self.episodes_list.append(Episode(str('1'), url, thumb, background))
+            return
+
+        # Parse url of episodes
+        info = OrderedDict()
+        for tag in content.find_all('div'):
+            if tag.string:
+                continue
+            id = tag.get('id')
+            if not id:
+                continue
+
+            if tag.find('div', 'gd_thumb'):
+                url, thumb, background = self.__parse_arch_1(tag)
+                if not url and not thumb and not background:
+                    continue
+
+            elif tag.find('a'):
+                url, thumb, background = self.__parse_arch_2(tag)
+                if not url and not thumb and not background:
+                    continue
+            else:
+                #TODO: another arch type
+                continue
+            info[id] = [url, thumb, background]
+
+        # Parse number of episodes
+        number_dict = {}
+        for tag in content.find_all('a'):
+            href = tag.get('href')
+            if not href:
+                continue
+            if '#' not in href:
+                continue
+            tab = href.split('#')[-1]
+            tab_content = tag.string.encode('utf8')
+            number_dict[tab] = tab_content
+
+        # Strip invalid tab
+        for id in info:
+            number = number_dict.get(id)
+            if not number:
+                continue
+            url, thumb, background = info.get(id)
+            # print number, url, background, thumb
+            self.episodes_list.append(Episode(number, url, thumb, background))
+
+    def get_list(self):
+        return self.episodes_list
+
+
+class Anime:
+    def __init__(self, name = None, url = None, thumb = None):
+        self.name = self._to_str_type(name)
+        self.url = self._to_str_type(url)
+        self.thumb = self._to_str_type(thumb)
+
+    def _to_str_type(self, value):
+        if not value:
+            return
+        if isinstance(value, str):
+            return value
+        return value.encode('utf8')
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def thumb(self):
+        return self.__thumb
+
+    @thumb.setter
+    def thumb(self, value):
+        self.__thumb = value
+
+    @property
+    def url(self):
+        return self.__url
+
+    @url.setter
+    def url(self, value):
+        self.__url = value
+
+
+class ParserAnimeFrom2dGate:
+    def __init__(self):
+        self.target_url = 'http://2d-gate.org/forum-78-1.html'
+        self.first_page = self.target_url
+        self.class_data ={}
+
+    @property
+    def next_page(self):
+        return self.__next_page
+
+    @next_page.setter
+    def next_page(self, value):
+        self.__next_page = value
+
+    @property
+    def last_page(self):
+        return self.__last_page
+
+    @last_page.setter
+    def last_page(self, value):
+        self.__last_page = value
+
+    def __process_animes(self, url):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+
+        # Find next page
+        nex_tag = soup.find('a', 'nxt')
+        if nex_tag:
+            self.next_page = nex_tag.get('href').encode('utf8')
+        else:
+            self.next_page = None
+
+        # Find last page
+        last_tag = soup.find('a', 'last')
+        if last_tag:
+            self.last_page = last_tag.get('href').encode('utf8')
+        else:
+            self.last_page = None
+
+        animes = []
+        for tag in soup.find_all('a', 'z'):
+            name = tag.get('title')
+            url = tag.get('href')
+
+            img_tag = tag.find('img')
+            if not img_tag:
+                continue
+            img_str = img_tag.get('src')
+            img = img_str.split('src=')[-1]
+            animes.append(Anime(name=name, url=url, thumb=img))
+        return animes
+
+    def get_page(self, url):
+        return self.__process_animes(url)
+
+    def __process_html(self, url):
+        self.req = requests.get(url)
+        self.soup = BeautifulSoup(self.req.text, 'html.parser')
+        self.classes = self.soup.find('table', id='olAL')
+
+    def __get_thumb(self, url):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        thumb = soup.find('meta', property='og:image').get('content')
+        if not thumb:
+            return ''
+        else:
+            return thumb
+
+    def __process_content(self):
+        for item in self.classes:
+            class_name = item.find('th').text.encode('utf8')
+            if not self.class_data.has_key(class_name):
+                self.class_data.setdefault(class_name, [])
+            contenet = self.class_data.get(class_name)
+
+            for unit in item.find_all('a'):
+                name = unit.find('div').next_element
+                url = BUILD_URL(unit.get('href'))
+                contenet.append(Anime(name, url))
+
+    def get_classes(self):
+        self.__process_html(self.target_url)
+
+        if not self.class_data.has_key('classes'):
+            self.class_data.setdefault('classes', [])
+        classes = self.class_data.get('classes')
+        for item in self.classes:
+            classes.append(item.find('th').text.encode('utf8'))
+        return classes
+
+    def get_content(self, value):
+        self.get_classes()
+
+        name_list = []
+        if not self.class_data.has_key(value):
+            self.__process_content()
+
+        content = self.class_data.get(value)
+        for anime in content:
+            name_list.append(anime.name)
+        return name_list
+
+    def get_thumb(self, class_name, anime_name):
+        if not self.class_data.has_key(class_name):
+            return ''
+
+        content = self.class_data.get(class_name)
+        for anime in content:
+            if anime_name != anime.name:
+                continue
+            if not anime.thumb:
+                anime.thumb = self.__get_thumb(anime.url)
+            return anime.thumb
+        return ''
+
+    def get_url(self, class_name, anime_name):
+        if not self.class_data.has_key(class_name):
+            return ''
+
+        content = self.class_data.get(class_name)
+        for anime in content:
+            if anime_name != anime.name:
+                continue
+            return '' if not anime.url else anime.url
+
+    def get_target_url(self):
+        return self.target_url
+
+
+if __name__ == '__main__':
+    obj = ParserAnimeFrom2dGate()
+
